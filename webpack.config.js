1835cbe7877c3f7812e384e57293473e92867365
Motiejus <motiejus.labzentis@gmail.com>
2019-11-22 22:30:37 +0200

    finished

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          'postcss-sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|mp4|webm)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+};
